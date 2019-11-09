65c64f0a5262604475a34e00936ff4ae80bfa1ca
VixelDisc <vixeldisc@gmail.com>
2019-11-09 19:00:36 +0000

    Day 4 Update 2

---
+++ commands/warn.js
@@ -0,0 +1,72 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const ms = require("ms")
+let warns = JSON.parse(fs.readFileSync("../data/warnings.json", "utf8"));
+
+module.exports.run = async (client, message, args) => {
+
+    let dmauthW = message.author;
+
+    if(!message.member.hasPermission("268435456")) return dmauthW.send("You do not have the required Permissions!");
+    let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+    if(!wUser) return message.reply("I could not find a user with that name, or you did not specify one.")
+    if(wUser.hasPermission("268435456")) return message.reply("This person can not be warned at your permission level.");
+    let reason = args.join(" ").slice(22)
+    if(!reason) return message.reply("You did not specify a reason.")
+
+    if(!warns[wUser.id]) warns[wUser.id] = {
+        warns: 0
+    }
+
+    warns[wUser.id].warns++;
+
+    fs.writeFile("../data/warnings.json", JSON.stringify(warns), (err) => {
+        if(err) console.log(err)
+    });
+
+    let warnEmbed = new Discord.RichEmbed()
+    .setDescription("Warn")
+    .setAuthor(message.author.username)
+    .setColor("#fc6400")
+    .addField("Warned User", `<@${wUser.id}>`)
+    .addField("Warned In", message.channel)
+    .addField("Number of Warnings", warns[wUser.id].warns)
+    .addField("Reason", reason);
+
+    let warnchannel = message.guild.channels.find(`name`, "logs");
+    if(!warnchannel) return message.reply("Could not find a Channel Named 'logs' Please Create One");
+
+    warnchannel.send(warnEmbed);
+
+    if(warns[wUser.id].warns === 238746){
+        let muterole = message.guild.role.find(`name`, "muted")
+        if(!muterole) {
+            try{
+        muterole = await message.guild.createRole({
+          name: "Muted",
+          mentionable: true,
+          color: "#595959",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+            } catch(err) {
+            console.log(err);
+            }
+        }
+
+        let mutetime = "5m";
+
+        await(wUser.addRole(muterole.id));
+
+        setTimeout(function(){
+            wUser.removeRole(muterole.id)
+        })
+    }
+
+
+}
